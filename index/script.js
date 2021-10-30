dd6f2ece298eb7d62e3ef16b68541416354c81de
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-10-30 20:40:50 +0100

    create start button

--- index/script.js
+++ index/script.js
@@ -1,4 +1,8 @@
-.start-page {
-    
-
+const startBtn = document.querySelector("#start-btn");
+const startPage = document.querySelector(".start-page");
+function exitStartPage() {
+  startBtn.addEventListener("click", () => {
+    startPage.style.visibility = "hidden";
+  });
 }
+exitStartPage();
