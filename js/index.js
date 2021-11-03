a48ba3ebc3912c4654e43028ede826e8a4dda567
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-11-03 18:56:55 +0000

    mvp

--- js/index.js
+++ js/index.js
@@ -1,22 +1,53 @@
-const startBtn = document.querySelector("#start-btn");
+const startBtn = document.querySelector(".start-btn");
 const startPage = document.querySelector(".start-page");
 const gamePage = document.querySelector(".game-page");
 const scoreText = document.querySelector("#score div span");
 const bill = document.querySelector(".bill");
+const popUp = document.querySelector(".popUp");
+const popUpText = document.querySelector(".popUp h1");
+const endLevelBtn = document.querySelector("#endLevelBtn");
+const nextLevelBtn = document.querySelector("#nextLevel");
 let scoreArr = [];
 let score = 0;
+let counter = 0;
+let lastBill = 0;
+let lastBillCounter = 0;
 let countdownFinished = false;
-
+const levels = {
+  1: {
+    name: "LEVEL 1",
+    minScore: 2000,
+    speed: 2000,
+  },
+  2: {
+    name: "LEVEL 2",
+    minScore: 2300,
+    speed: 1500,
+  },
+  3: {
+    name: "LEVEL 3",
+    minScore: 2700,
+    speed: 1000,
+  },
+};
+let level = 1;
 // FUNCTIONS
 function startGame() {
-  let counter = 5;
+  getScore(score, level);
+  let countDownCounter = 4;
+  let levelName = levels[level].name;
   let countdownNumber = document.createElement("h1");
   countdownNumber.id = "countdown";
   gamePage.appendChild(countdownNumber);
   const countdown = setInterval(() => {
-    countdownNumber.innerText = `${counter}`;
-    counter--;
-    if (counter < -1) {
+    if (countDownCounter === 4) {
+      countdownNumber.innerText = `${levelName}`;
+      countDownCounter--;
+    } else {
+      countdownNumber.innerText = `${countDownCounter}`;
+      countDownCounter--;
+    }
+    if (countDownCounter < -1) {
       clearInterval(countdown);
       countdownNumber.remove();
       playGame();
@@ -36,8 +67,9 @@ function getRandomPosition() {
   return selectedPosition;
 }
 
-function getRandomValue(randomCloud) {
-  let randomValue = getRandomNumber(3);
+function getRandomValue(randomCloud, level) {
+  // let randomValue = getRandomNumber(3);
+  let randomValue = 3;
   if (randomCloud.style.visibility === "hidden") {
     if (randomValue === 1) {
       randomCloud.style.backgroundImage = "url('/images/billete-100.jpg')";
@@ -47,7 +79,8 @@ function getRandomValue(randomCloud) {
       randomCloud.onclick = function () {
         randomCloud.style.visibility = "hidden";
         score += 100;
-        getScore(score);
+        lastBill = 100;
+        getScore(score, level);
       };
     } else if (randomValue === 2) {
       randomCloud.style.backgroundImage = "url('/images/billete-200.jpg')";
@@ -57,7 +90,8 @@ function getRandomValue(randomCloud) {
       randomCloud.onclick = function () {
         randomCloud.style.visibility = "hidden";
         score += 200;
-        getScore(score);
+        lastBill = 200;
+        getScore(score, level);
       };
     } else if (randomValue == 3) {
       randomCloud.style.backgroundImage = "url('/images/billete-500.jpg')";
@@ -67,39 +101,87 @@ function getRandomValue(randomCloud) {
       randomCloud.onclick = function () {
         randomCloud.style.visibility = "hidden";
         score += 500;
-        getScore(score);
+        lastBill = 500;
+        if (lastBill === 500) {
+          lastBillCounter++;
+        } else {
+          lastBillCounter = 0;
+        }
+        instantLose();
+        getScore(score, level);
       };
     }
   }
 }
 
 function playGame() {
+  score = 0;
+  counter = 0;
   let intervalId;
-  let counter = 0;
   let expiredBill = null;
   intervalId = setInterval(() => {
     let cloudPosition = getRandomPosition();
-    getRandomValue(cloudPosition);
+    getRandomValue(cloudPosition, level);
     counter++;
     if (!expiredBill) {
       expiredBill = cloudPosition;
     } else {
       if (expiredBill == cloudPosition) {
-        expiredBill = cloudPosition;
+        // expiredBill = cloudPosition;
         return;
       }
       expiredBill.style.visibility = "hidden";
       expiredBill = cloudPosition;
     }
-
-    if (counter === 30) {
+    if (counter >= 9) {
+      console.log("my score is", score);
       clearInterval(intervalId);
       expiredBill.style.visibility = "hidden";
+      if (score < levels[level].minScore) {
+        level++;
+        popUp.style.visibility = "visible";
+        endLevelBtn.className = `playAgain`;
+        endLevelBtn.innerText = "PLAY AGAIN";
+        popUpText.innerText = `GAME OVER`;
+        const playAgainBtn = document.querySelector(".playAgain");
+        playAgainBtn.onclick = function () {
+          window.location.reload();
+        };
+      } else {
+        if (level < 3) {
+          console.log("ganaste primer nivel");
+          level++;
+          popUp.style.visibility = "visible";
+          endLevelBtn.className = `level${level}`;
+          endLevelBtn.innerText = "PLAY";
+          popUpText.innerText = `LEVEL ${level}`;
+          const level2Btn = document.querySelector(`.level${level}`);
+          level2Btn.onclick = function () {
+            popUp.style.visibility = "hidden";
+            score = 0;
+            counter = 0;
+            getScore(score, level);
+            startGame();
+          };
+        } else {
+          score = 0;
+          popUp.style.visibility = "visible";
+          popUpText.innerText = `CONGRATULATIONS YOU WON!`;
+          endLevelBtn.className = "playAgain";
+          endLevelBtn.innerText = "PLAY AGAIN";
+          const playAgainBtn = document.querySelector(".playAgain");
+          playAgainBtn.onclick = function () {
+            window.location.reload();
+          };
+        }
+      }
     }
-  }, 4000);
+  }, levels[level].speed);
+  console.log(`mi final score is`, score);
+  return score;
 }
 
-function getScore(points) {
+function getScore(points, level) {
   scoreArr.push(points);
   if (
     scoreArr.length - 1 === 500 && //<--- revisar
@@ -107,20 +189,25 @@ function getScore(points) {
     scoreArr.length - 3 === 500
   ) {
     // score += points;
-    scoreText.innerText = points;
+    scoreText.innerText = `${points}/${levels[level].minScore}`;
     instantLose();
   } else {
     console.log(`my score is ${points}`);
-    scoreText.innerText = points;
+    scoreText.innerText = `${points}/${levels[level].minScore}`;
   }
 }
 function instantLose() {
-  // logica para perder por coger 3 de 500€
+  if (lastBillCounter === 3) {
+    console.log("looooser!");
+  }
 }
 
 window.addEventListener("load", () => {
+  document.getElementById("player").play();
   startBtn.addEventListener("click", () => {
     startPage.style.visibility = "hidden";
     startGame();
   });
+
+  // playGame.onclick();
 });
