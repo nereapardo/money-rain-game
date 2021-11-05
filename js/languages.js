664793f4c3814efb3b4e70e58a753bea4d44e08a
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-11-05 07:45:48 +0000

    change instructions text + incompatibility with smaller screens

--- js/languages.js
+++ js/languages.js
@@ -1,7 +1,7 @@
 const gameInstructions = {
   description: {
-    es: `Quien dijo que el dinero no cae del cielo? En este juego la lluvia cae en forma de billetes de 100€, 200€ y 500€. Recoge tantos billetes como puedas y pasa los 3 niveles para hacerte rico. Por cada nivel aumentará el mínimo de dinero a recoger y la velocidad. Intenta llenar tu cartera al máximo, pero ten cuidado, porque la avaricia rompe el saco.`,
-    en: " Who said that money does not fall from the sky? In this game the rain falls in the form of 100€, 200€ and 500€ bills. Collect as many bills as you can and pass the 3 levels to get rich. For each level, the minimum amount of money to collect and the speed will increase. Try to fill your wallet to the maximum, but be careful, because greed breaks the sack.",
+    es: `Quien dijo que el dinero no cae del cielo? En este juego la lluvia cae en forma de billetes de 100€, 200€ y 500€. Haz click en los billetes para recoge tantos como puedas.Tu misión es pasar los 3 niveles y hacerte rico. Por cada nivel aumentará el mínimo de dinero a recoger y la velocidad. Intenta llenar tu cartera al máximo, pero ten cuidado, porque la avaricia rompe el saco.`,
+    en: " Who said that money does not fall from the sky? In this game the rain falls in the form of 100€, 200€ and 500€ bills. Click to collect as many bills as you can. Your mission is to pass the 3 levels and get rich. For each level, the minimum amount of money to collect and the speed will increase. Try to fill your wallet to the maximum, but be careful, because greed breaks the sack.",
   },
   easterEeg: {
     es: "Has perdido! Ya te advertí que la codicia rompe el saco. Cada vez que coges 3 billetes de 500€ pierdes, por lo que no seas avaricioso y déjalo caer.",
